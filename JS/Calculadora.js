45af4066884a7a46c159b57be8215e0371f8e55a
Wilson Ricardo Humerez Hidalgo <rickbel_94@outlook.com>
2018-11-13 16:58:11 -0400

    commit 2

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -50,7 +50,7 @@ function teclado(elEvento) {
     //teclas númericas del teclado alfamunérico
     if (k > 47 && k < 58) {
         p = k - 48; //buscar número a mostrar.
-        p = String(p) //convertir a cadena para poder añádir en pantalla.
+        p = String(p);//convertir a cadena para poder añádir en pantalla.
         numero(p); //enviar para mostrar en pantalla
     }
     //Teclas del teclado númerico. Seguimos el mismo procedimiento que en el anterior.
@@ -59,13 +59,13 @@ function teclado(elEvento) {
         p = String(p);
         numero(p);
     }
-    if (k == 110 || k == 190) { numero(".") } //teclas de coma decimal
-    if (k == 106) { operar('*') } //tecla multiplicación
-    if (k == 107) { operar('+') } //tecla suma
-    if (k == 109) { operar('-') } //tecla resta
-    if (k == 111) { operar('/') } //tecla división
-    if (k == 32 || k == 13) { igualar() } //Tecla igual: intro o barra espaciadora
-    if (k == 46) { borradoTotal() } //Tecla borrado total: "supr"
-    if (k == 8) { retro() } //Retroceso en escritura : tecla retroceso.
-    if (k == 36) { borradoParcial() } //Tecla borrado parcial: tecla de inicio.
+    if (k == 110 || k == 190) { numero(".");} //teclas de coma decimal
+    if (k == 106) { operar('*');} //tecla multiplicación
+    if (k == 107) { operar('+');} //tecla suma
+    if (k == 109) { operar('-');} //tecla resta
+    if (k == 111) { operar('/');} //tecla división
+    if (k == 32 || k == 13) { igualar();} //Tecla igual: intro o barra espaciadora
+    if (k == 46) { borradoTotal();} //Tecla borrado total: "supr"
+    if (k == 8) { retro();} //Retroceso en escritura : tecla retroceso.
+    if (k == 36) { borradoParcial();} //Tecla borrado parcial: tecla de inicio.
 }
